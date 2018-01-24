0109c7a386133e1daccd51f4686909770b299afd
fengxingqian <15832926436@163.com>
2018-01-24 15:00:15 +0800

    e3-manager分soa后第一次提交

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,8 +1,10 @@
 package cn.e3mall.service;
 
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 	
 	public TbItem findItemById(long id);
+	public DataGridResult pageQueryItems(int page,int rows);
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -13,6 +20,22 @@ public class ItemServiceImpl implements ItemService {
 	@Override
 	public TbItem findItemById(long id) {
 		return itemMapper.selectByPrimaryKey(id);
+	}
+	@Override
+	public DataGridResult pageQueryItems(int page, int rows) {
+		DataGridResult result = new DataGridResult();
+		//设置分页数据
+		PageHelper.startPage(page, rows);
+		//执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		//取查询结果
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		//获取总数
+		long total = pageInfo.getTotal();
+		result.setRows(list);
+		result.setTotal(total);
+		return result;
 	}
 
 }
